0bbbfee21175e18664f0b8431910bcd61fca2ed6
sadique <1234sadique1234@gmail.com>
2021-01-23 13:46:43 +0530

    update

--- backend/config/config.js
+++
@@ -1,9 +0,0 @@
-const env = process.env.NODE_ENV||'development';
-
-var config =require('./config.json');
-
-var envConfg = config[env];
-
-Object.keys(envConfg).forEach(key =>{
-  process.env[key]=envConfg[key]
-})
