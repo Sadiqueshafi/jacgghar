6333899f34d8c7d8541a208138f3776aa87b9f41
sadique <1234sadique1234@gmail.com>
2021-01-31 22:40:18 +0530

    p

--- backend/app.js
+++ backend/app.js
@@ -56,6 +56,10 @@ else{
 
 })
 //step:3
+app.get('/*', function(req,res) {
+
+  res.sendFile(path.join(__dirname+'./dist/herohu/index.html'));
+  });
 
 if(process.env.NODE_ENV ==='production'){
   app.use(express.static('./dist/herohu'))
